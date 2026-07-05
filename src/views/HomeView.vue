3f761ddf894292251da3b28ff14d5343dce4bdae
unknown <hatef.dev21@gmail.com>
2026-07-05 20:23:56 +0330

    WIP

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,17 +12,41 @@
           آسان‌تر از همیشه می‌کنند.
         </p>
       </div>
-      <div class=""></div>
+      <div class="w-full grid grid-cols-2 h-[500px]">
+        <div class="grid grid-cols-3">
+          <div class="col-span-2 flex justify-between flex-col h-full bg-white rounded-2xl p-6">
+            <div
+              class="w-16 h-16 rounded-full bg-[#f7f7f7] flex justify-center items-center flex-col"
+            >
+              <WrenchScrewdriverIcon class="w-8 h-8" />
+            </div>
+            <div class="flex justify-between">
+              <div class="flex flex-col gap-y-4">
+                <h2 class="text-black font-semibold text-3xl">قطعات موتوری</h2>
+                <span class="text-[#575757] text-base"> قطعه موتوری خودروی را بیشتر ببینید </span>
+              </div>
+            </div>
+          </div>
+          <div class="col-span-1"></div>
+        </div>
+        <div class="grid grid-cols-3">
+          <div class="col-span-2 flex flex-col"></div>
+          <div class="col-span-1"></div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script>
 import Header from "@/components/Header.vue";
 import Banner from "@/components/Banner.vue";
+import { WrenchScrewdriverIcon } from "@heroicons/vue/24/outline";
 export default {
+  name: "HomeView",
   components: {
     Header,
     Banner,
+    WrenchScrewdriverIcon,
   },
 };
 </script>
