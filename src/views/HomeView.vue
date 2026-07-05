e395e7ace907aac01096ec949b50a098e22d6702
unknown <hatef.dev21@gmail.com>
2026-07-05 19:57:00 +0330

    add header of the catergory section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,18 @@
   <div class="flex flex-col gap-y-2">
     <Header />
     <Banner />
+    <div class="flex flex-col gap-y-4 mt-10 container">
+      <div class="flex flex-col gap-y-6 justify-center items-center">
+        <h1 class="text-black font-light text-4xl">
+          خرید با <span class="font-semibold">دسته بندی</span>
+        </h1>
+        <p class="text-[#575757] text-center text-lg w-2xl">
+          قطعه یدکی مناسب خودروی خود را سریع‌تر پیدا کنید؛ با دسته‌بندی‌هایی هوشمند که انتخاب را
+          آسان‌تر از همیشه می‌کنند.
+        </p>
+      </div>
+      <div class=""></div>
+    </div>
   </div>
 </template>
 <script>
