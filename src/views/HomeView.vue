56b64c84f501beac1fdd6b39a444e2d27550c3b0
unknown <hatef.dev21@gmail.com>
2026-07-07 13:50:17 +0330

    add

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,22 +12,57 @@
           آسان‌تر از همیشه می‌کنند.
         </p>
       </div>
-      <div class="w-full grid grid-cols-2 h-[500px]">
-        <div class="grid grid-cols-3">
-          <div class="col-span-2 flex justify-between flex-col h-full bg-white rounded-2xl p-6">
+      <div class="w-full grid grid-cols-2 h-[400px]">
+        <div class="gap-x-2 flex">
+          <router-link
+            to="/motor-parts"
+            class="group w-7/12 cursor-pointer hover:bg-black transition-all duration-300 flex justify-between flex-col h-full bg-white rounded-4xl p-6"
+          >
             <div
-              class="w-16 h-16 rounded-full bg-[#f7f7f7] flex justify-center items-center flex-col"
+              class="w-16 h-16 rounded-full bg-[#f7f7f7] group-hover:bg-[#171717] transition-all duration-300 flex justify-center items-center flex-col"
             >
-              <WrenchScrewdriverIcon class="w-8 h-8" />
+              <WrenchScrewdriverIcon class="w-8 h-8 group-hover:text-white" />
             </div>
-            <div class="flex justify-between">
-              <div class="flex flex-col gap-y-4">
-                <h2 class="text-black font-semibold text-3xl">قطعات موتوری</h2>
+
+            <div class="flex flex-col gap-y-4">
+              <h2
+                class="text-black font-semibold text-3xl transition-all duration-300 group-hover:text-white"
+              >
+                قطعات موتوری
+              </h2>
+              <div class="flex justify-between items-center">
+                <span class="text-[#575757] text-base"> قطعه موتوری خودروی را بیشتر ببینید </span>
+                <div
+                  class="w-12 group-hover:scale-120 group bg-[#000000] group-hover:bg-[#171717] transition-all duration-300 h-12 rounded-full flex justify-center items-center flex-col"
+                >
+                  <ArrowLeftIcon class="w-6 h-6 text-white" />
+                </div>
+              </div>
+            </div>
+          </router-link>
+
+          <router-link
+            to="/motor-parts"
+            class="w-5/12 group relative cursor-pointer hover:bg-black transition-all duration-300 flex justify-between flex-col h-full bg-white rounded-4xl p-6"
+          >
+            <div class="flex flex-col gap-y-4">
+              <h2
+                class="text-black font-semibold text-3xl transition-all duration-300 group-hover:text-white"
+              >
+                قطعات موتوری
+              </h2>
+              <div class="flex justify-between items-center">
                 <span class="text-[#575757] text-base"> قطعه موتوری خودروی را بیشتر ببینید </span>
               </div>
             </div>
-          </div>
-          <div class="col-span-1"></div>
+            <div class="flex justify-end">
+              <div
+                class="w-12 group-hover:scale-120 group bg-[#000000] group-hover:bg-[#171717] transition-all duration-300 h-12 rounded-full flex justify-center items-center flex-col"
+              >
+                <ArrowLeftIcon class="w-6 h-6 text-white" />
+              </div>
+            </div>
+          </router-link>
         </div>
         <div class="grid grid-cols-3">
           <div class="col-span-2 flex flex-col"></div>
@@ -41,12 +76,14 @@
 import Header from "@/components/Header.vue";
 import Banner from "@/components/Banner.vue";
 import { WrenchScrewdriverIcon } from "@heroicons/vue/24/outline";
+import { ArrowLeftIcon } from "@heroicons/vue/24/outline";
 export default {
   name: "HomeView",
   components: {
     Header,
     Banner,
     WrenchScrewdriverIcon,
+    ArrowLeftIcon,
   },
 };
 </script>
