aca8f343bb2cd4a4eb488e89c391d50050622c6b
unknown <hatef.dev21@gmail.com>
2026-07-09 12:26:33 +0330

    WIP

--- src/components/DiscountBanner.vue
+++ src/components/DiscountBanner.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-black">
-    <div class="container py-10">
+    <div class="container py-10 flex flex-col gap-y-5">
       <h1 class="text-white text-4xl font-bold">پیشنهادهای شگفت‌انگیز هفته</h1>
       <DiscountTimeLeft :source="['12', '00', '00']" />
     </div>
--- src/components/DiscountTimeLeft.vue
+++ src/components/DiscountTimeLeft.vue
@@ -1,10 +1,15 @@
 <template>
-  <div v-for="value in source" class="flex items-center">
-    <div class="bg-yellow-300 rounded-full w-12 h-12 flex justify-center items-center">
+  <div class="flex items-center gap-x-2">
+    <div
+      v-for="value in source"
+      :key="value"
+      class="bg-yellow-300 rounded-full w-12 h-12 flex justify-center items-center"
+    >
       <span class="text-black">{{ value }}</span>
     </div>
   </div>
 </template>
++
 <script>
 export default {
   name: "DiscountTimeLeft",
