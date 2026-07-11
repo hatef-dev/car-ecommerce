1398b37cd2a7da75d7e426fc9192d6a5908892eb
unknown <hatef.dev21@gmail.com>
2026-07-11 11:37:27 +0330

    add banner

--- src/components/BannerSite.vue
+++ src/components/BannerSite.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="container">
     <div
-      class="bg-[url(@/assets/banner.png)] w-full h-[400px] rounded-3xl bg-cover bg-center bg-no-repeat flex justify-center items-center"
+      class="bg-[url(@/assets/banner.png)] w-full h-[400px] rounded-3xl bg-cover bg-center bg-no-repeat flex flex-col gap-y-10 justify-center items-center"
     >
       <h2 class="text-white text-4xl font-bold w-1/4 text-center leading-10">
         نمایندگی قطعات یدکی خودرو وارد کننده از چین
       </h2>
+      <ButtonComponent>الان خرید کنید</ButtonComponent>
     </div>
   </div>
 </template>
